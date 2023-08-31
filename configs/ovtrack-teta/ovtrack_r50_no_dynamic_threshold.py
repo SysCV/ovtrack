cf0041b38b0bef47f80b9ad0297bca74eeaeeed2
siyliepfl <lisiyu@student.ethz.ch>
2023-08-31 23:28:05 +0200

    update the ovtrack training

--- configs/ovtrack-teta/ovtrack_r50_no_dynamic_threshold.py
+++ configs/ovtrack-teta/ovtrack_r50_no_dynamic_threshold.py
@@ -277,7 +277,7 @@ lr_config = dict(
     warmup_ratio=1.0 / 1000,
     step=[3, 5])
 total_epochs = 6
-load_from = 'saved_models/pretrained/clip_head.pth'
+load_from = 'saved_models/pretrained_models/ovtrack_clip_distillation.pth'
 evaluation = dict(metric=['track'], start=5, interval=1, resfile_path='/scratch/tmp/')
 
 checkpoint_config = dict(interval=1, create_symlink=False)
